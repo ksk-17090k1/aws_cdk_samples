d8cf26f141d2fb019a640d8ec0e2d57fa25dd94a
inagaki-keisuke <kin71796@gmail.com>
2024-06-30 20:13:45 +0900

    chore: Update makefile and lambda stack

--- my-project/lib/my-lambda-stack.ts
+++ my-project/lib/my-lambda-stack.ts
@@ -1,8 +1,6 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-import * as aws_lambda from "aws-cdk-lib/aws-lambda";
-import { aws_apigateway } from "aws-cdk-lib";
-import { aws_lambda_nodejs } from "aws-cdk-lib";
+import { aws_apigateway, aws_lambda_nodejs, aws_lambda } from "aws-cdk-lib";
 
 export class MyLambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -26,6 +24,8 @@ export class MyLambdaStack extends cdk.Stack {
         functionName: "TypeScriptHandler",
         entry: "./src/lambda/ts-hello.ts",
         handler: "handler",
+        timeout: cdk.Duration.seconds(30),
+        memorySize: 512,
         runtime: aws_lambda.Runtime.NODEJS_20_X,
       }
     );
@@ -33,7 +33,7 @@ export class MyLambdaStack extends cdk.Stack {
     // NOTE: Lambdaと統合前提ならLambdaRestApiの方がコード量減っていいかも
     const restApi = new aws_apigateway.RestApi(this, "MyRestApi", {
       deployOptions: {
-        stageName: "v1",
+        stageName: "v2",
       },
       restApiName: `myRestApi`,
     });
@@ -44,8 +44,36 @@ export class MyLambdaStack extends cdk.Stack {
       proxy: true,
     });
 
-    const restApiHelloWorld = restApi.root.addResource("hello");
-    restApiHelloWorld.addMethod("GET", integration);
-    restApiHelloWorld.addMethod("POST", integration);
+    const books = restApi.root.addResource("books");
+    const getMethod = books.addMethod("GET", integration);
+    const postMethod = books.addMethod("POST", integration, {
+      apiKeyRequired: true,
+    });
+
+    const book = books.addResource("{id}");
+    book.addMethod("GET", integration);
+
+    // 使用料プランを作って、API keyとstageに紐づける
+    const plan = restApi.addUsagePlan("UsagePlan", {
+      name: "Normal",
+      throttle: {
+        rateLimit: 20,
+        burstLimit: 200,
+      },
+    });
+    plan.addApiKey(restApi.addApiKey("apiKey"));
+    plan.addApiStage({
+      stage: restApi.deploymentStage,
+      // メソッドレベルでのスロットリング (optional)
+      throttle: [
+        {
+          method: postMethod,
+          throttle: {
+            rateLimit: 10,
+            burstLimit: 20,
+          },
+        },
+      ],
+    });
   }
 }
