e16c53d3b84b73b3c3c9786e738d2867029c5e38
inagaki-keisuke <kin71796@gmail.com>
2024-09-06 23:45:58 +0900

    chore: Update npm dependency to latest stable version

--- my-project/lib/constructs/ecs/my-ecr.ts
+++ my-project/lib/constructs/ecs/my-ecr.ts
@@ -3,6 +3,8 @@ import * as cdk from "aws-cdk-lib";
 import * as ecr from "aws-cdk-lib/aws-ecr";
 import { DockerImageAsset, Platform } from "aws-cdk-lib/aws-ecr-assets";
 import * as ecrdeploy from "cdk-ecr-deployment";
+import * as imagedeploy from "cdk-docker-image-deployment";
+import * as ecs from "aws-cdk-lib/aws-ecs";
 
 type Props = {
   accountId: string | undefined;
@@ -43,7 +45,7 @@ export class MyEcr extends Construct {
       imageTagMutability: ecr.TagMutability.IMMUTABLE,
     });
 
-    // Create Docker Image Asset
+    // --- cdk-ecr-deployment を使う場合 ---
     const dockerImageAsset = new DockerImageAsset(
       this,
       `${resourceName}DockerImageAsset`,
@@ -53,13 +55,39 @@ export class MyEcr extends Construct {
       }
     );
 
-    // Deploy Docker Image to ECR Repository
-    const tagName = "v1";
+    // NOTE: DockerImageAsset はハッシュ値を持つ
+    // TODO: ほんとはコミットハッシュをタグにするのが一番良さそうだが、、、
+    //       actionsで ${{ github.sha }} を cdk deploy の引数に渡すでいけそう
+    const tagName = `prod-${dockerImageAsset.assetHash}`;
     new ecrdeploy.ECRDeployment(this, `${resourceName}DeployDockerImage`, {
       src: new ecrdeploy.DockerImageName(dockerImageAsset.imageUri),
       dest: new ecrdeploy.DockerImageName(
         `${accountId}.dkr.ecr.${region}.amazonaws.com/${ecrRepository.repositoryName}:${tagName}`
       ),
+    });
+
+    // --- cdk-docker-image-deployment を使う場合 ---
+    // 結論、cdk-ecr-deployment を使うほうがよいかと思う
+    // こっちのほうが cdk-ecr-deployment よりは簡潔にかけるが、DockerImageAsset を使わないのでhash値を取得できない
+    // NOTE: 厳密には、destinationで指定するtagを省略すると、内部で DockerImageAsset を使ってhash値がタグになるが、
+    //       内部で使われているだけなので、外から取得できない
+    const tagName2 = "stg-djflivfdnlvnl";
+    new imagedeploy.DockerImageDeployment(
+      this,
+      "ExampleImageDeploymentWithTag",
+      {
+        source: imagedeploy.Source.directory("./lib/constructs/ecs/"),
+        destination: imagedeploy.Destination.ecr(ecrRepository, {
+          // ここを省略すると、DockerImageAsset で計算されたhash値がタグになる
+          tag: tagName2,
+        }),
+      }
+    );
+
+    // こんな感じで任意のリポジトリでタスク定義に指定できる
+    const taskDef = new ecs.FargateTaskDefinition(this, "TaskDef", {});
+    taskDef.addContainer("AppContainer", {
+      image: ecs.ContainerImage.fromEcrRepository(ecrRepository, tagName),
     });
   }
 }
