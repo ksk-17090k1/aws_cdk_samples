33ca834d7ed09ab38082bc3634c04942c99169f6
inagaki-keisuke-lvgs <keisuke.inagaki@leverages.jp>
2024-12-04 14:45:02 +0900

    feat: Refactor MyBasicVpc to simplify VPC creation logic

--- my-project/lib/constructs/vpc/my-basic-vpc.ts
+++ my-project/lib/constructs/vpc/my-basic-vpc.ts
@@ -10,21 +10,20 @@ export class MyBasicVpc extends Construct {
     super(scope, id);
 
     // VPCは既存のものを利用する場合もけっこうあるのでこのような分岐の書き方は有効的。
-    if (props.vpcId != null) {
-      const vpc = ec2.Vpc.fromLookup(this, "MyVpc", { vpcId: props.vpcId });
-    } else {
-      const vpc = new ec2.Vpc(this, "MyVpc", {
-        vpcName: "MyVpc",
-        // 192.168.0.0/16, 172.16.0.0/16 でもOK
-        ipAddresses: ec2.IpAddresses.cidr("10.0.0.0/16"),
-        // it defaults to DEFAULT
-        defaultInstanceTenancy: ec2.DefaultInstanceTenancy.DEFAULT,
-        // 以下2つの詳細はメモ参照。defaultは両者ともtrue
-        enableDnsHostnames: true,
-        enableDnsSupport: true,
-        // it defaults to 3
-        maxAzs: 3,
-      });
-    }
+    const vpc =
+      props.vpcId != null
+        ? ec2.Vpc.fromLookup(this, "MyVpc", { vpcId: props.vpcId })
+        : new ec2.Vpc(this, "MyVpc", {
+            vpcName: "MyVpc",
+            // 192.168.0.0/16, 172.16.0.0/16 でもOK
+            ipAddresses: ec2.IpAddresses.cidr("10.0.0.0/16"),
+            // it defaults to DEFAULT
+            defaultInstanceTenancy: ec2.DefaultInstanceTenancy.DEFAULT,
+            // 以下2つの詳細はメモ参照。defaultは両者ともtrue
+            enableDnsHostnames: true,
+            enableDnsSupport: true,
+            // it defaults to 3
+            maxAzs: 3,
+          });
   }
 }
